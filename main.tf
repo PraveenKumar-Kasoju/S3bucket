554fc760f74e6e9e8d8e755df25668b6acc966fb
PraveenKumar-Kasoju <60596414+PraveenKumar-Kasoju@users.noreply.github.com>
2024-02-06 10:57:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
-provider "aws" {
-  region = "us-east-1"  # Change to your desired region
-}
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "MyVPC"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.5.0"
+    }
   }
+}
+provider "aws" {
+  region = var.region
 }
